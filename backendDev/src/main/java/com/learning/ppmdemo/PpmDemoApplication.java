17029d39c55e928d0d38dbef2fd92419c25fe06c
Vladica Deljanin <vladica.deljanin@gmail.com>
2019-12-02 15:19:09 +0100

    first commit on backend

---
+++ backendDev/src/main/java/com/learning/ppmdemo/PpmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.learning.ppmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmDemoApplication.class, args);
+    }
+
+}
---
+++ backendDev/src/test/java/com/learning/ppmdemo/PpmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.ppmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
